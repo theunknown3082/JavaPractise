af1e6934b79a5bd7c06c73f12fd0559b8cbb621f
theunown3082 <theunknownlol049@gmail.com>
2023-10-26 16:32:07 +0530

    error handling ||alter||

--- src/Main.java
+++ src/Main.java
@@ -832,3 +832,25 @@ public class Main {
     }
 }
 */
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueInput = true;
+
+        do {
+            try {
+                System.out.println("Enter an integer: ");
+                int number = scanner.nextInt();
+                System.out.println("The entered number is: " + number);
+                continueInput = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Try again. Incorrect input: an integer is required");
+                scanner.nextLine();
+            }
+        } while (continueInput);
+
+        scanner.close();
+    }
+}
